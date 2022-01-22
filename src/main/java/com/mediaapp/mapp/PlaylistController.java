843c41b75dca40ab60539b757a5c3168dbcb4d4b
Chinaka I Light <chinakalight@googlemail.com>
2022-01-22 13:58:07 -0600

    testing endpoints using intelij http file

---
+++ src/main/java/com/mediaapp/mapp/PlaylistController.java
@@ -0,0 +1,62 @@
+package com.mediaapp.mapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RequestMapping("/playlist")
+@RestController
+public class PlaylistController {
+
+    @RequestMapping("/{id}")
+    public String getAllPlaylists(@PathVariable long id) {
+        return "GetAllPlaylists (Id): " + id;
+    }
+
+    @RequestMapping("/namedVariable/{uId}")
+    public String getAllSavedPlaylists(@PathVariable("uId") long uniqueId){
+        return "Get All Saved Playlists (UniqueId): " + uniqueId;
+    }
+
+    @RequestMapping(value = {"/album"})
+    public Object[] getOneSavedPlaylist() {
+        String[] albumSubList = {"Rock-Album", "Blues-Album"};
+        return Arrays.asList("Rocking-Album", "Blueing-Album", albumSubList).toArray();
+//        return albumSubList;
+    }
+
+    @RequestMapping(method = {RequestMethod.GET}, value = {"reqCall"})
+    public String getRequestCall(){
+        return "From RequestCall Method";
+    }
+
+    @RequestMapping(value = {"argPassing"})
+    public Object[] getParamsCalled(@RequestParam("name") String name, @RequestParam("address") String address) {
+        return Arrays.asList(name, address).toArray();
+    }
+
+    @RequestMapping(value = {"argPassing2"}, params = {"name", "address"})
+    public Object[] getParamsCalled2(@RequestParam("name") String name, @RequestParam("address") String address) {
+        return Arrays.asList(name, address).toArray();
+    }
+
+    @RequestMapping(value = "reqHeader", headers = {"content-type=application/json", "accept=application/json" })
+    public String[] getHeadersCalled(){
+        return new String[] {"Heading1", "Heading2"};
+    }
+
+    @RequestMapping(value = "consumeReq", consumes = {"text/plain", "application/*" } )
+    public String[] getRequestMappingConsumes(){
+        return new String[]{"Consuming Text/Plain", "Consuming/Applicatin./*" };
+    }
+
+    @RequestMapping(value = "produceReq", produces = {"text/plain", "application/*"})
+    public String[] producesReqApp() {
+        return new String[]{"Plain", "text", "Producing RequestMapping Handler"};
+    }
+
+
+}
